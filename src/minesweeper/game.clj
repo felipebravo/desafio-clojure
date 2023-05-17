0b4d906fbfa9bcb44f851a2cbc73de2f32975700
felipebravo <felipe_bravo@hotmail.com>
2023-05-16 22:37:52 -0300

    feat: add record

--- src/minesweeper/game.clj
+++ src/minesweeper/game.clj
@@ -3,20 +3,33 @@
             [clojure.string :as str]
             [minesweeper.board :as board]))
 
+(defn formatted-time [time]
+  (quot time 1000))
+
+(defrecord GameRecord [record time])
+
+(defonce record (atom (GameRecord. nil nil)))
+
+(defonce best-record (atom nil))
+
 (defn start-game []
   (load-file "board.clj")
   (println "OBS: O indice das linhas e das colunas inicia em 0.")
   (println "- Use '(place-flag indice_LINHA indice_COLUNA)' para colocar uma bandeira.")
   (println "- Use '(remove-flag indice_LINHA indice_COLUNA)' para remover uma bandeira.")
   (println "- Use '(explore indice_LINHA indice_COLUNA)' para revelar um indice.")
-  (pprint board/undisclosed-board))
+  (pprint board/undisclosed-board)
+  (reset! record (GameRecord. @board/undisclosed-board (System/currentTimeMillis)))
+  (println "Tempo da partida: Iniciado"))
 
 (defn explore [row col]  
   (let [index-value (get-in board/board-to-play [row col])]
     (if (= index-value :M)
       (do
         (println "!BOOM! Voce foi derrotado !BOOM!")
-        (println "- Use '(start-game)' para reiniciar."))
+        (println "- Use '(start-game)' para reiniciar.")
+        (let [game-time (formatted-time (- (System/currentTimeMillis) (:time @record)))]
+          (println "Tempo da partida: "game-time" segundos")))
       (do
         (swap! board/undisclosed-board
                (fn [board]
@@ -24,11 +37,26 @@
         (doseq [row-vec @board/undisclosed-board]
           (println (str/join " " row-vec)))))))
 
+(defn handle-record [] 
+  (let [current-time (System/currentTimeMillis) 
+        game-time (formatted-time (- current-time (:time @record)))
+        best-record (formatted-time (when (:record @record) (:time @record)))] 
+        (if (or (nil? best-record) (< game-time best-record))
+          (do
+            (reset! record (GameRecord. @board/undisclosed-board game-time))
+            (println "Parabens, voce venceu! Novo recorde registrado: "game-time" segundos."))
+          (do
+            (println "Parabens, voce venceu! ! !")))
+        (println "Tempo da partida: "game-time" segundos"))
+  )
+
 (defn check-win-by-flag [board]
-  (let [matched-indices (filter #(and (= (@board %) '[F]) (= (board/board-to-play %) :M)) (range (count @board)))]
-    (if (= (count matched-indices) 4)
-      (println (count matched-indices))
-      (println "Parabéns, você venceu!!!"))))
+  (let [flattened-board (flatten board)
+        flattened-board-to-play (flatten board/board-to-play)
+        matched-elements (filter (fn [[x y]] (and (= x 'F) (= y :M)))
+                                (map vector flattened-board flattened-board-to-play))]
+    (if (= (count matched-elements) 4)
+       (handle-record))))
 
 (defn place-flag [row col]
   (let [updated-row (assoc (get @board/undisclosed-board row) col '[F])
